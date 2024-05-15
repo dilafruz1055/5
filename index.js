e71118149ecc10a6c4bf53126b3019cdba44e7a5
Dilafruz Shukurova <dilafruzshukurova36@gmail.com>
2024-05-15 11:42:53 +0500

    dan

---
+++ index.js
@@ -0,0 +1,12 @@
+let sonda = Number(prompt("Hfta kinunu sonda kiriting"));
+
+switch(sonda) {
+    case 1: alert("Dushanba"); break;
+    case 2: alert("Seshanba"); break;
+    case 3: alert("Chorshanba"); break;
+    case 4: alert("Payshanba"); break;
+    case 5: alert("Juma"); break;
+    case 6: alert("Shanba"); break;
+    case 7: alert("Yakshanba"); break;
+    
+}
